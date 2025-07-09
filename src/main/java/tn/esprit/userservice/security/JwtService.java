b6f226e51c2bbafd59938fb5638ad375e97f99de
Maram-web <maram.manai@esprit.tn>
2025-07-09 17:00:47 +0200

    costumer user

--- src/main/java/tn/esprit/userservice/security/JwtService.java
+++ src/main/java/tn/esprit/userservice/security/JwtService.java
@@ -1,53 +1,53 @@
-package tn.esprit.userservice.security;
+    package tn.esprit.userservice.security;
 
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
-import io.jsonwebtoken.security.Keys;
-import jakarta.servlet.http.HttpServletRequest;
-import org.springframework.stereotype.Service;
-import tn.esprit.userservice.entity.User;
+    import io.jsonwebtoken.Jwts;
+    import io.jsonwebtoken.SignatureAlgorithm;
+    import io.jsonwebtoken.security.Keys;
+    import jakarta.servlet.http.HttpServletRequest;
+    import org.springframework.stereotype.Service;
+    import tn.esprit.userservice.entity.User;
 
-import java.security.Key;
-import java.util.Date;
+    import java.security.Key;
+    import java.util.Date;
 
-@Service
-public class JwtService {
-    private static final String SECRET = "MySuperSecretKeyWith256BitsLength!!!!!!!";
-    private final Key key = Keys.hmacShaKeyFor(SECRET.getBytes());
+    @Service
+    public class JwtService {
+        private static final String SECRET = "MySuperSecretKeyWith256BitsLength!!!!!!!";
+        private final Key key = Keys.hmacShaKeyFor(SECRET.getBytes());
 
-   // private final Key key = Keys.secretKeyFor(SignatureAlgorithm.HS256);
-    private final long expirationTimeMs = 1000 * 60 * 60 * 10; // 10h
+       // private final Key key = Keys.secretKeyFor(SignatureAlgorithm.HS256);
+        private final long expirationTimeMs = 1000 * 60 * 60 * 10; // 10h
 
-    public String generateToken(User user) {
-        return Jwts.builder()
-                .setSubject(user.getUsername()) // ✅ maintenant on utilise le username
-                .claim("email", user.getEmail()) // 👉 si tu veux garder l'email
-                .claim("role", user.getRole())
+        public String generateToken(User user) {
+            return Jwts.builder()
+                    .setSubject(user.getUsername()) // ✅ maintenant on utilise le username
+                    .claim("email", user.getEmail()) // 👉 si tu veux garder l'email
+                    .claim("role", user.getRole())
 
-                .setIssuedAt(new Date())
-                .setExpiration(new Date(System.currentTimeMillis() + expirationTimeMs))
-                .signWith(key)
-                .compact();
-    }
-
-
-    public String extractUsername(String token) {
-        return Jwts.parserBuilder()
-                .setSigningKey(key)
-                .build()
-                .parseClaimsJws(token)
-                .getBody()
-                .getSubject();
-    }
-
-    public String extractTokenFromRequest(HttpServletRequest request) {
-        String bearer = request.getHeader("Authorization");
-        if (bearer != null && bearer.startsWith("Bearer ")) {
-            return bearer.substring(7);
+                    .setIssuedAt(new Date())
+                    .setExpiration(new Date(System.currentTimeMillis() + expirationTimeMs))
+                    .signWith(key)
+                    .compact();
         }
-        return null;
+
+
+        public String extractUsername(String token) {
+            return Jwts.parserBuilder()
+                    .setSigningKey(key)
+                    .build()
+                    .parseClaimsJws(token)
+                    .getBody()
+                    .getSubject();
+        }
+
+        public String extractTokenFromRequest(HttpServletRequest request) {
+            String bearer = request.getHeader("Authorization");
+            if (bearer != null && bearer.startsWith("Bearer ")) {
+                return bearer.substring(7);
+            }
+            return null;
+        }
+
+
+
     }
-
-
-
-}
